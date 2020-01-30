5f0bba4bfa22c52d3c6ba73b3f13562a57906a00
jhelzer3 <59981853+jhelzer3@users.noreply.github.com>
2020-01-30 15:54:48 -0600

    Update Program.cs

--- WeatherCalculator2/WeatherCalculator2/Program.cs
+++ WeatherCalculator2/WeatherCalculator2/Program.cs
@@ -8,28 +8,27 @@ namespace WeatherCalculator2
         {
             Console.WriteLine("### Weather Calculator 2");
 
-            int temperature;
-            int windSpeed;
-            double windChill;
-            string input = "";
 
-            // Get the temperature from the user
-            Console.WriteLine("Enter the temperature");
+            string input = "";
+            
+
+            
+            Console.WriteLine("Please enter the temperature");
 
             input = Console.ReadLine();
 
-            temperature = int.Parse(input);
+            int temperature = int.Parse(input);
 
             // Get the windspeed from the user
-            Console.WriteLine("Enter the wind speed");
+            Console.WriteLine("Please nter the wind speed");
 
             input = Console.ReadLine();
 
-            windSpeed = int.Parse(input);
+            int windSpeed = int.Parse(input);
 
             // Calculate the wind chill
 
-            windChill = 35.74 + (0.6125 * temperature) - 35.75 * Math.Pow(windSpeed, 0.16) + 0.4275 * temperature * Math.Pow(windSpeed, 0.16);
+            double windChill = 35.74 + (0.6125 * temperature) - 35.75 * Math.Pow(windSpeed, 0.16) + 0.4275 * temperature * Math.Pow(windSpeed, 0.16);
 
             Console.WriteLine("The wind chill is " + windChill);
         }
